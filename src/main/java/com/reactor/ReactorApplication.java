b350072112065ddb3d72e83d5b75b7bcde800018
Eduardo Ceolin <eduardo_ceolin@terceiros.sicredi.com.br>
2019-05-01 13:08:03 -0300

    Starter

---
+++ src/main/java/com/reactor/ReactorApplication.java
@@ -0,0 +1,16 @@
+package com.reactor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReactorApplication.class, args);
+    }
+
+    public void test() {
+
+    }
+}
